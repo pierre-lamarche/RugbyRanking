47565c0685d3cb5d87aadacf130fe72b9921d86a
pierre-lamarche <lamarche.p@gmail.com>
2017-02-15 18:52:36 +0100

    Update scrap teams

--- Scrap teams.R
+++ Scrap teams.R
@@ -1,8 +1,11 @@
 getTeamsRugby <- function() {
   json_file <- fromJSON(file = "http://cmsapi.pulselive.com/rugby/country.json&page=0")
-  nPages <- ceiling(json_file$pageInfo$numEntries/100)
+  ### value labels for the factors
+  sportLabels <- do.call("c",json_file$content$sportLookup)
+  typeLabels <- do.call("c",json_file$content$typeLookup)
+  nPages <- ceiling(json_file$pageInfo$numEntries/10)
   for (k in 1:nPages) {
-    json_file <- fromJSON(file = paste0("http://cmsapi.pulselive.com/rugby/country.json&pageSize=100&page=",k))
+    json_file <- fromJSON(file = paste0("http://cmsapi.pulselive.com/rugby/country.json&pageSize=10&page=",k))
     json_file <- lapply(json_file$content$countries, function(x) {
       x[sapply(x, is.null)] <- NA
       unlist(x)
@@ -18,5 +21,11 @@ getTeamsRugby <- function() {
       dataRugby <- as.data.frame(do.call("rbind", json_file)) else
         dataRugby <- rbind(dataRugby, as.data.frame(do.call("rbind", json_file)))
   }
+  dataRugby$teams.sport <- factor(dataRugby$teams.sport,
+                                  levels = 1:length(sportLabels),
+                                  labels = sportLabels)
+  dataRugby$teams.type <- factor(dataRugby$teams.type,
+                                 levels = 1:length(typeLabels),
+                                 labels = typeLabels)
   return(dataRugby)
 }
