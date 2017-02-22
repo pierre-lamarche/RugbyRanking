52d674f60bf8ed70b84006a874c26793d6ed59ae
pierre-lamarche <lamarche.p@gmail.com>
2017-02-22 21:33:31 +0100

    ranking historic

--- Scrap historical ranking.R
+++ Scrap historical ranking.R
@@ -1,3 +1,5 @@
+library(dplyr)
+
 second_step <- 60*60*24*5
 date_start <- as.POSIXct("15/01/2004", format = "%d/%m/%Y")
 date_end <- as.POSIXct("20/02/2017", format = "%d/%m/%Y")
@@ -8,3 +10,15 @@ array_date <- as.Date(as.POSIXct(seq(from = as.numeric(date_start),
 
 json <- lapply(array_date, getRankingRugby)
 rankingHistoric <- do.call(json, "rbind")
+
+rankingFR <- filter(rankingHistoric,
+                    team.id == "42")
+
+plot(rankingFR$date, rankingFR$pts, type = "l", col = "blue", lwd = 2)
+plot(rankingFR$date, rankingFR$pos, type = "l", lty = "longdash", col = "lightblue", lwd = 2)
+
+rankingNZ <- filter(rankingHistoric,
+                    team.id == "37")
+
+plot(rankingNZ$date, rankingNZ$pts, type = "l", col = "blue", lwd = 2)
+plot(rankingNZ$date, rankingNZ$pos, type = "l", lty = "longdash", col = "lightblue", lwd = 2)
